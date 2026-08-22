eb2f26812201eb30460a25ef6b2bca686bc60726
Mohd Altamish <144524570+MohdAltamish@users.noreply.github.com>
2026-08-23 00:34:04 +0530

    Change video object-fit from cover to contain

--- components/intro/IntroScreen.tsx
+++ components/intro/IntroScreen.tsx
@@ -94,13 +94,14 @@ export default function IntroScreen({
           inset-0
           h-full
           w-full
-          object-cover
+          object-contain
+          md:object-cover
           transition-opacity
           duration-500
           ${isReady ? "opacity-100" : "opacity-0"}
         `}
-        // src="/videos/shrinik-intro.mp"
-           src="https://res.cloudinary.com/dgrzeojzd/video/upload/v1787422660/shrinik-intro_xxvbv5.mp4"
+       // src="/videos/shrinik-intro.mp4"
+          src="https://res.cloudinary.com/dgrzeojzd/video/upload/v1787422660/shrinik-intro_xxvbv5.mp4"
         autoPlay
         muted
         playsInline
